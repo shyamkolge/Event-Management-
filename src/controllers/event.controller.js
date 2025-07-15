7ddd594d1cba4e0c8786dfe42e644ad4fbf077f4
Shyam Kolge <shyamnkolge2020@gmail.com>
2025-07-15 23:41:08 +0530

    get event details by id done

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -21,4 +21,21 @@ const createEvent = asyncHandler(async (req, res, next) => {
     .json(new ApiResponce(201, { eventId: event.id }, "Event created"));
 });
 
-export { createEvent };
+const getEventDetails = asyncHandler(async (req, res, next) => {
+  const id = req.params.eventId;
+  console.log(id);
+
+  const [event] = await sql`SELECT * FROM events WHERE id = ${id}`;
+  if (!event) return next(new ApiError(404, "Event not found"));
+
+  const users = await sql`
+    SELECT u.id, u.name, u.email
+    FROM event_registrations er
+    JOIN users u ON er.user_id = u.id
+    WHERE er.event_id = ${id};
+  `;
+
+  res.json(new ApiResponce(200, { ...event, registeredUsers: users }));
+});
+
+export { createEvent, getEventDetails };
--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,8 +1,12 @@
-import { createEvent } from "../controllers/event.controller.js";
+import {
+  createEvent,
+  getEventDetails,
+} from "../controllers/event.controller.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/create", createEvent);
+router.get("/getEventDetails/:eventId", getEventDetails);
 
 export default router;
