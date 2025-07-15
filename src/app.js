3e85945dcb7bf8de09a2d9b159ca10cf78ae8195
Shyam Kolge <shyamnkolge2020@gmail.com>
2025-07-16 01:37:37 +0530

    final modifications are done

--- src/app.js
+++ src/app.js
@@ -1,27 +1,51 @@
 import express from "express";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import authRouter from "./routes/auth.routes.js";
 import isLoggedIn from "./middlewares/isLoggedIn.js";
 import cookieParser from "cookie-parser";
 import eventRouter from "./routes/event.routes.js";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import swaggerUi from "swagger-ui-express";
+import morgan from "morgan";
+import YAML from "yamljs";
+import helmet from "helmet";
 
-const app = express();
-
+// Dotenv Configuration
 import path from "path";
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
+const app = express();
+
+// Global Middlewares
+// Sets the Security HTTP Headers
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "126kb" }));
 app.use(cookieParser());
 
-app.get("/", isLoggedIn, (req, res) => {
-  res.send(process.env.PORT);
+const limiter = rateLimit({
+  max: 2,
+  windowMs: 60 * 60 * 1000,
+  message: "To many requiest from this IP, please try again after an hour",
 });
 
+app.use("", limiter);
+
+// HTTP Logger
+app.use(morgan("combined"));
+
+// API Documentation
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Auth Routes
 app.use("/api/v1/auth", authRouter);
+
+// Event Routes
 app.use("/api/v1/event", isLoggedIn, eventRouter);
 
+// Global Error Handler
 app.use(globalErrorHandler);
 
 export default app;
